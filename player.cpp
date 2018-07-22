84dbc6804a4e2338709b0314017cd332cb1e0db7
F74066129 <f74066129@mail.ncku.edu.com>
2018-07-22 16:14:30 +0800

    player.cpp

---
+++ player.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "1 9 4 2 8 6 3 C" << endl;
+
+    while(getline(cin,s)) {
+        cout << "1 1 5 13" << endl;
+        cout << "1 9 3 1" << endl;
+        cout << "1 C 19 13" << endl;
+        cout << "1 4 17 13" << endl;
+        cout << "1 2 2 1" << endl;
+        cout << "1 8 5 13" << endl;
+        cout << "1 3 2 15" << endl;
+        cout << "1 6 16 11" << endl;
+        cout << "0" << endl;
+    }
+    return 0;
+}
